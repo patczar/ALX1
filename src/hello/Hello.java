a4925ce88bbfe984ad572d239fd674f634f44640
Patryk Czarnik <patryk@patrykczarnik.net>
2022-09-14 15:03:11 +0200

    Dodatkowe linie w Hello world

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,9 +1,13 @@
 package hello;
 
+import java.time.LocalTime;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Druga linia");
+		System.out.println("Godzina " + LocalTime.now());
 	}
 
 }
