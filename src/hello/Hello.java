3f477008d79b299238b76647901d0715e6455c20
Patryk Czarnik <patryk@patrykczarnik.net>
2022-09-14 15:52:37 +0200

    Wyświetlanie również daty.

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Hello {
@@ -7,6 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Druga linia");
+		System.out.println("Data " + LocalDate.now());
 		System.out.println("Godzina " + LocalTime.now());
 	}
 
