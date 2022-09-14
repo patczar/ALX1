31aad698dd1399948fde3c50656d83dddfd11fd6
Patryk Czarnik <patryk@patrykczarnik.net>
2022-09-14 15:06:05 +0200

    Program dla Osoby

---
+++ src/klasy/Program.java
@@ -0,0 +1,12 @@
+package klasy;
+
+import java.time.LocalDate;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Osoba ala = new Osoba("Ala", "Kowalska", LocalDate.of(1990, 1, 1));
+		System.out.println(ala);
+	}
+
+}
